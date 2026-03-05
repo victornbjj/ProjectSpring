fe9bd0724dc5ef129b450833dceffd186c816bfe
J. Victor Pereira <victorifpb78@gmail.com>
2026-03-05 13:39:24 -0300

    inicial commit

---
+++ todolist/src/main/java/br/com/vfit/todolist/user/UserModel.java
@@ -0,0 +1,12 @@
+package br.com.vfit.todolist.user;
+
+import lombok.Data;
+
+@Data
+public class UserModel {
+    private String username;
+    private String name; 
+    private  String password;
+
+
+}
---
+++ todolist/src/test/java/br/com/vfit/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vfit.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
