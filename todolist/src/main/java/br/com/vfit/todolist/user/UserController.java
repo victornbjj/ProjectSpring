ee945e97cd8b93a230e39c39d1f57905c741a861
J. Victor Pereira <victorifpb78@gmail.com>
2026-03-07 21:32:20 -0300

    feat: created db h2

--- todolist/src/main/java/br/com/vfit/todolist/user/UserController.java
+++ todolist/src/main/java/br/com/vfit/todolist/user/UserController.java
@@ -1,5 +1,6 @@
 package br.com.vfit.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,12 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
-    @PostMapping("/")
-    public void create(@RequestBody UserModel userModel){
-        System.out.println(userModel.getUsername());
+    @Autowired
+    private IUserRepository userRepository;
 
+    @PostMapping("/")
+    public UserModel create(@RequestBody UserModel userModel) {
+        var userCreated = this.userRepository.save(userModel);
+        return userCreated;
 
     }
-    
 
 }
--- todolist/src/main/java/br/com/vfit/todolist/user/UserModel.java
+++ todolist/src/main/java/br/com/vfit/todolist/user/UserModel.java
@@ -1,12 +1,29 @@
 package br.com.vfit.todolist.user;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Data;
 
+@Entity(name="tb_users")
 @Data
 public class UserModel {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+
     private String username;
-    private String name; 
+    private String name;
     private  String password;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 
 
 }
