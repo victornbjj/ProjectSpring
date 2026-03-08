21f3c8bff5a8c0157a505deafcd67a95198c5ce7
J. Victor Pereira <victorifpb78@gmail.com>
2026-03-07 23:27:30 -0300

    feat: userName validation

--- todolist/src/main/java/br/com/vfit/todolist/user/UserModel.java
+++ todolist/src/main/java/br/com/vfit/todolist/user/UserModel.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -17,11 +18,12 @@ public class UserModel {
     @Id
     @GeneratedValue(generator = "UUID")
     private UUID id;
-
+    
+    @Column(unique = true)
     private String username;
     private String name;
     private  String password;
-
+    
     @CreationTimestamp
     private LocalDateTime createdAt;
 
