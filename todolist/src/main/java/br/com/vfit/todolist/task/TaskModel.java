9ce31ffbf0dae6414059044a3cc7161266208fa5
J. Victor Pereira <victorifpb78@gmail.com>
2026-03-08 15:24:48 -0300

    feat:Creating task files

--- todolist/src/main/java/br/com/vfit/todolist/task/TaskModel.java
+++ todolist/src/main/java/br/com/vfit/todolist/task/TaskModel.java
@@ -25,7 +25,7 @@ public class TaskModel {
     @Column(length=50)
     private String title;
     private LocalDateTime startAt;
-    private LocalDateTime endAT;
+    private LocalDateTime endAt;
     private String priority;
 
     @CreationTimestamp
