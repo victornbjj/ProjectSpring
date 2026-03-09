7a29bfb5c03e73382b33a34cc318409e9ed9b72e
J. Victor Pereira <victorifpb78@gmail.com>
2026-03-08 22:27:13 -0300

    "feat: list and tasks"

--- todolist/src/main/java/br/com/vfit/todolist/task/ITaskRepository.java
+++ todolist/src/main/java/br/com/vfit/todolist/task/ITaskRepository.java
@@ -1,9 +1,10 @@
 package br.com.vfit.todolist.task;
 
 import java.util.UUID;
-
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface  ITaskRepository extends  JpaRepository<TaskModel, UUID>{
+public interface ITaskRepository extends JpaRepository<TaskModel, UUID> {
 
+    List<TaskModel> findByIdUser(UUID idUser);
 }
--- todolist/src/main/java/br/com/vfit/todolist/task/TaskController.java
+++ todolist/src/main/java/br/com/vfit/todolist/task/TaskController.java
@@ -1,15 +1,13 @@
 package br.com.vfit.todolist.task;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import jakarta.servlet.http.HttpServletRequest;
 
@@ -23,7 +21,7 @@ public class TaskController {
     @PostMapping("/")
     public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
 
-       
+
         var idUser = request.getAttribute("idUser");
         taskModel.setIdUser((UUID)idUser);
 
@@ -39,6 +37,12 @@ public class TaskController {
         var task = this.taskRepository.save(taskModel);
         return  ResponseEntity.status(HttpStatus.OK).body(task);
 
+    }
+    @GetMapping("/")
+    public List<TaskModel> list(HttpServletRequest request){
+        var IdUser = request.getAttribute("idUser");
+         var tasks =  this.taskRepository.findByIdUser((UUID)IdUser);
+        return tasks;
     }
 
 }
