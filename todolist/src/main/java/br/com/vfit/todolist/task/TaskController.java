0643faa43c1a0d09c8ea865c535c91e972642f6e
J. Victor Pereira <victorifpb78@gmail.com>
2026-03-08 21:51:27 -0300

    fix: validetion hours from tasks

--- todolist/src/main/java/br/com/vfit/todolist/task/TaskController.java
+++ todolist/src/main/java/br/com/vfit/todolist/task/TaskController.java
@@ -1,8 +1,11 @@
 package br.com.vfit.todolist.task;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,13 +21,23 @@ public class TaskController {
     private ITaskRepository taskRepository;
 
     @PostMapping("/")
-    public TaskModel create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
+    public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
 
        
         var idUser = request.getAttribute("idUser");
         taskModel.setIdUser((UUID)idUser);
+
+        var currentDate = LocalDateTime.now();
+
+        if(currentDate.isAfter(taskModel.getStartAt()) || currentDate.isAfter(taskModel.getEndAt()) ){
+            return  ResponseEntity.status(HttpStatus.BAD_REQUEST).body("A data de inicio/ data de termino de ser maior que a data atual");
+        }
+        if(taskModel.getStartAt().isAfter(taskModel.getEndAt())){
+            return  ResponseEntity.status(HttpStatus.BAD_REQUEST).body("A data de inicio deve ser menor data de termino");
+        }
+
         var task = this.taskRepository.save(taskModel);
-        return task;
+        return  ResponseEntity.status(HttpStatus.OK).body(task);
 
     }
 
